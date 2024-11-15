c201d3c2d5ab70177fd25eaf63a440bee37305f4
Alberto Valera <153525133+aalbertovalera48@users.noreply.github.com>
2024-11-15 19:29:29 +0100

    Nuevos cambios html y controllers

--- src/main/java/com/example/jurassicpark/controller/ViewController.java
+++ src/main/java/com/example/jurassicpark/controller/ViewController.java
@@ -1,22 +1,28 @@
 package com.example.jurassicpark.controller;
 
-import com.example.jurassicpark.service.SensorService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
 
 @Controller
 public class ViewController {
 
-    @Autowired
-    private SensorService sensorService;
+    @GetMapping("/")
+    public String homeView() {
+        return "home";
+    }
 
     @GetMapping("/monitor")
     public String monitorView() {
         return "monitor";
     }
-}
 
+    @GetMapping("/dinosaurios")
+    public String dinosauriosView() {
+        return "dinosaurios";
+    }
+
+    @GetMapping("/enfermeria")
+    public String enfermeriaView() {
+        return "enfermeria";
+    }
+}
